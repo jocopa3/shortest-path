89fe2dbec192fce58662816580dc332487c41dcb
jocopa3 <jocopa3@users.noreply.github.com>
2023-08-02 20:12:32 -0700

    Minor API logging message changes hide-marker param now expected to be a valid boolean Supress unchecked cast warnings

--- src/main/java/shortestpath/ShortestPathAPI.java
+++ src/main/java/shortestpath/ShortestPathAPI.java
@@ -17,19 +17,20 @@ public class ShortestPathAPI {
 		this.shortestPathPlugin = plugin;
 	}
 
+	@SuppressWarnings("unchecked")
 	private static <T> T getValidatedObject(Object object, Class<T> expectedType) {
-		if (object == null || !expectedType.isInstance(object)) {
+		if (!expectedType.isInstance(object)) {
 			return null;
 		}
 		return (T) object;
 	}
 
-	public void parseDataMap(Map data) {
+	public void parseDataMap(Map<?, ?> data) {
 		Plugin plugin;
 		if (data.containsKey("plugin")) {
 			plugin = getValidatedObject(data.get("plugin"), Plugin.class);
 			if (plugin == null) {
-				log.debug("Received data without a valid Plugin object");
+				log.debug("Received data without a valid 'plugin' object type");
 				return;
 			}
 		} else {
@@ -40,7 +41,7 @@ public class ShortestPathAPI {
 
 		String command = getValidatedObject(data.get("command"), String.class);
 		if (command == null) {
-			log.debug("{} - Received data without a valid command object", requester);
+			log.debug("{} - Received data without a valid 'command' object", requester);
 			return;
 		}
 
@@ -53,14 +54,14 @@ public class ShortestPathAPI {
 		switch (command) {
 			case "request-path": handleRequestPath(requester, data); break;
 			case "clear-path": handleClearPath(requester); break;
-			default: log.debug("{} - Invalid command: ", requester, command);
+			default: log.debug("{} - Invalid command: {}", requester, command);
 		}
 	}
 
-	private void handleRequestPath(PluginIdentifier requester, Map data) {
+	private void handleRequestPath(PluginIdentifier requester, Map<?, ?> data) {
 		WorldPoint target = getValidatedObject(data.get("target"), WorldPoint.class);
 		if (target == null) {
-			log.debug("{} - Target is either not set or contains an invalid object", requester);
+			log.debug("{} - 'target' is either not set or contains an invalid object type", requester);
 			return;
 		}
 
@@ -68,13 +69,21 @@ public class ShortestPathAPI {
 		if (data.containsKey("start")) {
 			Object startObj = data.get("start");
 			if (startObj != null && !(startObj instanceof WorldPoint)) {
-				log.debug("{} - Start contained an invalid object", requester);
+				log.debug("{} - 'start' contained an invalid object type", requester);
 				return;
 			}
 			start = getValidatedObject(startObj, WorldPoint.class);
 		}
 
-		boolean hideMarker = data.containsKey("hide-marker");
+		boolean hideMarker = false;
+		if (data.containsKey("hide-marker")) {
+			Boolean hideMarkerObj = getValidatedObject(data.get("hide-marker"), Boolean.class);
+			if (hideMarkerObj == null) {
+				log.debug("{} - 'hide-marker' contained an invalid object type", requester);
+				return;
+			}
+			hideMarker = Boolean.TRUE.equals(hideMarkerObj);
+		}
 
 		PathParameters parameters = new PathParameters(requester, start, target, hideMarker, true);
 		shortestPathPlugin.requestPath(parameters);
